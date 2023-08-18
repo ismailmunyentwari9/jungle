7785a0707236ed069258452447f8223c29262871
Ismail <ismailmunyentwari9@gmail.com>
2023-08-18 10:38:16 +0200

    Add logo and single vido

--- src/components/HomeSlides.jsx
+++ src/components/HomeSlides.jsx
@@ -1,65 +1,47 @@
+/* eslint-disable no-template-curly-in-string */
 /* eslint-disable max-len */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/no-array-index-key */
-import React, { useState } from 'react';
+import Akagera from './assets/media/akagera.mp4';
 
-const slidesImages = [
-  {
-    url: 'https://i.gifer.com/7L35.gif',
-  },
-  {
-    url: 'https://78.media.tumblr.com/984d390d3c2244fc54152df451017b68/tumblr_p0o4dth8St1wbxqk4o1_540.gif',
-  },
-  {
-    url: 'https://media.tenor.com/5skqpqDytFkAAAAd/bull-moose-moose.gif',
-  },
-];
-
-const Slideshow = () => {
-  const [currentSlide, setCurrentSlide] = useState(1);
-
-  return (
-    <div className=" w-full">
-      <div className="w-full">
-        <div
-          className="w-full lg:h-[100vh] md:h-[60vh] bg-center bg-cover"
-          style={{ backgroundImage: `url(${slidesImages[currentSlide].url})` }}
+const Slideshow = () => (
+  <div className=" w-full">
+    <div className="w-full">
+      <div
+        className="w-full lg:h-[100vh] md:h-[0vh] bg-center bg-cover"
+      >
+        <video
+          autoPlay
+          muted
+          loop
+          style={{
+            width: '100%', height: '100%', objectFit: 'cover', position: 'absolute', zIndex: -1,
+          }}
         >
-          <div className="container mx-auto flex flex-col justify-center items-center lg:pt-[15%] md:pt-[10%]">
-            <p className="lg:text-[45px] md:text-[25px] text-white font-bold text-center">
-              {' '}
-              ADVENTURE IS WOTHWHILE
-            </p>
-            <p className="text-center text-[15px] text-white font-bold">
-              Discover New Places With Us, Adventure Awaits
-            </p>
-            <button
-              type="button"
-              className="bg-orange-500 text-white font-bold px-2 py-1 rounded-md lg:mt-5 "
-            >
-              Discover More
-            </button>
-          </div>
-          <div className=" left-0 right-0 flex justify-center items-center space-x-4 pb-3 mt-[7%]">
-            <div className="flex space-x-4 bg-gray-800 py-2 px-10 rounded-full">
-              {slidesImages.map((_, index) => (
-                <button
-                  key={index}
-                  className={`w-4 h-4 rounded-full ${
-                    index === currentSlide ? 'bg-orange-500' : 'bg-white'
-                  }`}
-                  onClick={() => setCurrentSlide(index)}
-                />
-              ))}
-            </div>
-          </div>
+          <source src={Akagera} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="container mx-auto flex flex-col justify-center items-center lg:pt-[15%] md:pt-[10%]">
+          <p className="lg:text-[45px] md:text-[25px] text-black font-bold text-center">
+            {' '}
+            ADVENTURE IS WOTHWHILE
+          </p>
+          <p className="text-[15px] text-orange-800 font-bold w-1-/9 lg:w-1/3 mg:w-1/3 ">
+            Discover New Places With Us, Where Every Journey Unveils a World of Excitement and Wonder. Embark on Adventures that Lead to Unforgettable Memories, and Embrace the Thrill of Exploring the Unknown. Your Next Epic Expedition Awaits, Ready to Ignite Your Passion for Discovery and Create Stories that Last a Lifetime.
+          </p>
+          <button
+            type="button"
+            className="bg-orange-500 text-white font-bold px-2 py-1 rounded-md lg:mt-5 "
+          >
+            Discover More
+          </button>
         </div>
       </div>
-
     </div>
-  );
-};
+
+  </div>
+);
 
 export default Slideshow;
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+import Logo from './assets/media/logo.jpeg';
 
 const Nav = () => (
   <div className="bg-gray-800 w-full   h-auto">
     <div className="container mx-auto  w-5/6 py-4  flex items-center justify-between md:gap-10">
       <div className="">
-        <span className="text-orange-500 lg:text-3xl md:text-2xl text-lg">Jungle</span>
-        <span className="lg:text-3xl md:text-2xl text-lg text-white">Trail</span>
+        <span className="text-orange-500"><img className="rounded-full w-[110px] h-[110px]" src={Logo} alt="logo" /></span>
       </div>
       <ul className="hidden lg:flex text-white text-lg gap-4 md:grid md:grid-cols-4">
         <li className="py-1 px-2 hover:bg-orange-400">Home</li>
