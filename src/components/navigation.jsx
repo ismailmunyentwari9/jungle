694307321e66dc6662a2faf2393a13a407e02513
Ismail <ismailmunyentwari9@gmail.com>
2023-09-04 12:09:05 +0200

    Add logo image to nav bar

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+import Logo from './assets/media/logo.jpeg';
 
 const Nav = () => (
   <div className="bg-gray-800 w-full   h-auto">
     <div className="container mx-auto  w-5/6 py-4  flex items-center justify-between md:gap-10">
       <div className="">
-        <a href="#logo" className="lg:w-1/9 md:col-span-1">
-          <span className="text-orange-500 lg:text-3xl text-lg">Jungle</span>
-          <span className="lg:text-3xl text-lg text-white">Trail</span>
+        <a href="#logo" className="lg:w-1/12 md:col-span-1">
+          <span className="text-orange-500 lg:text-3xl text-lg"><img src={Logo} alt="logo" className="w-[60px] h-[60px] rounded-full" /></span>
+
         </a>
       </div>
       <ul className="hidden lg:flex text-white text-lg gap-4 md:grid md:grid-cols-4">
